5eade8eda9a246ee0b99f18731e64a4e3fb30ba8
Luan Felix <prpluan@gmail.com>
2026-09-21 10:56:38 -0300

    Refactor constants for network and explorer URIs
    Removed old network and explorer URI logic, added default constants.

---
+++ src/lib/src/lib/common/constants.ts
@@ -0,0 +1,7 @@
+```ts
+export const DEFAULT_EXPLORER_URI_TX = "https://sigmaspace.io/en/transaction/";
+export const DEFAULT_EXPLORER_URI_TOKEN = "https://sigmaspace.io/en/token/";
+export const DEFAULT_EXPLORER_URI_ADDR = "https://sigmaspace.io/en/address/";
+
+export const DEFAULT_NETWORK = "mainnet";
+export const DEFAULT_EXPLORER_API = "https://api.ergoplatform.com";
